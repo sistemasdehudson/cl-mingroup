092685f658b38d5f68282d63692f61b742918dc1
sistemassdeh <63413763+sistemassdeh@users.noreply.github.com>
2020-12-15 18:54:53 -0300

    Update __manifest__.py

--- citval_default/__manifest__.py
+++ citval_default/__manifest__.py
@@ -30,7 +30,7 @@
 
     # list of url repos to install in the form 'repo-url directory'
     'git-repos': [
-        'https://github.com/regaby/cl-obladi.git',
+        'https://github.com/sistemasdehudson/cl-citval.git',
         'https://github.com/regaby/odoo-custom.git',
         'https://github.com/regaby/sdeh-pos.git',
         'https://github.com/jobiols/odoo-addons.git',
